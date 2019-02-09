746313233c21b5766e48dddcdf42b4f2d1bee7fa
JumbooWong <493182574@qq.com>
2019-02-09 09:44:51 +0800

    增加原子类操作

---
+++ src/main/java/com/sheliming/thread/atomic/ConcurrencyTest.java
@@ -0,0 +1,44 @@
+package com.sheliming.thread.atomic;
+
+import com.sheliming.thread.annoations.NotThreadSafe;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@NotThreadSafe
+public class ConcurrencyTest {
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+    public static int count = 0;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("count:" + count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
