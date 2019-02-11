b1fc70254b85e1a4122723682849cc6f4dd16270
shelimingming <shelimingming@qq.com>
2019-02-11 16:16:24 +0800

    long在32位虚拟机中的读写操作都不是原子性的

---
+++ src/main/java/com/sheliming/thread/longnoatom/Atomicity.java
@@ -0,0 +1,53 @@
+package com.sheliming.thread.longnoatom;
+
+public class Atomicity {
+    //静态变量t
+    public  static long t = 0;
+    //静态变量t的get方法,同步方法
+    public synchronized static long getT() {
+        return t;
+    }
+    //静态变量t的set方法，同步方法
+    public synchronized static void setT(long t) {
+        Atomicity.t = t;
+    }
+    //改变变量t的线程
+    public static class ChangeT implements Runnable{
+        private long to;
+        public ChangeT(long to) {
+            this.to = to;
+        }
+        public void run() {
+            //不断的将long变量设值到 t中
+            while (true) {
+                Atomicity.setT(to);
+                //将当前线程的执行时间片段让出去，以便由线程调度机制重新决定哪个线程可以执行
+                Thread.yield();
+            }
+        }
+    }
+    //读取变量t的线程，若读取的值和设置的值不一致，说明变量t的数据被破坏了，即线程不安全
+    public static class ReadT implements Runnable{
+
+        public void run() {
+            //不断的读取NotAtomicity的t的值
+            while (true) {
+                long tmp = Atomicity.getT();
+                //比较是否是自己设值的其中一个
+                if (tmp != 100L && tmp != 200L && tmp != -300L && tmp != -400L) {
+                    //程序若执行到这里，说明long类型变量t，其数据已经被破坏了
+                    System.out.println(tmp);
+                }
+                ////将当前线程的执行时间片段让出去，以便由线程调度机制重新决定哪个线程可以执行
+                Thread.yield();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new Thread(new ChangeT(100L)).start();
+        new Thread(new ChangeT(200L)).start();
+        new Thread(new ChangeT(-300L)).start();
+        new Thread(new ChangeT(-400L)).start();
+        new Thread(new ReadT()).start();
+    }
+}
